5600b05e36f0cbc9ca66e9297d938ee364aac6f8
LubomirP <lubomirpacheliev@gmail.com>
2021-09-17 16:10:08 +0300

    timer implementation and better naming

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import fetch from 'node-fetch';
 
-const purchaseURL = 'https://www.binance.com/bapi/nft/v1/private/nft/mystery-box/purchase';
+const PURCHASE_URL = 'https://www.binance.com/bapi/nft/v1/private/nft/mystery-box/purchase';
+const PRODUCT_ID = 133913760132809728
 
-const asyncGetBoxInfo = async (productID) => {
+const getBoxInfo = async productID => {
     try {
-        const infoURL = 'https://www.binance.com/bapi/nft/v1/friendly/nft/mystery-box/detail?productId=';
-        const res = await fetch(infoURL + productID);
+        const INFO_URL = 'https://www.binance.com/bapi/nft/v1/friendly/nft/mystery-box/detail?productId=';
+        const res = await fetch(INFO_URL + productID);
         if (!res.ok) throw new Error('Response is not okay');
         const parsedRes = await res.json();
         return parsedRes;
@@ -14,5 +15,16 @@ const asyncGetBoxInfo = async (productID) => {
     }
 }
 
-asyncGetBoxInfo('133913760132809728')
-.then(res => console.log(res.data.startTime))
+const startTimeout = async startTime => {
+    const currTime = new Date().getTime();
+    const boxInfo = await getBoxInfo(PRODUCT_ID);
+    const boxData = boxInfo.data;
+
+    setTimeout(() => purchaseBox(boxData), startTime - currTime);
+}
+
+const purchaseBox = async boxData => {
+    console.log('Mystery box purchased.')
+}
+
+startTimeout(new Date().getTime() + 2000);
